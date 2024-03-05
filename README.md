b45f9590ac1ee8c4017edfba97fba82e7423ddc3
Joshua <joshuakijkuit@hotmail.com>
2024-03-05 15:50:12 +0100

    Today we made a plan of approach, made a GitHub repository to store our program.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # TUBGOA
 The Ultimate Board Game
+
+## Log
+- 5-3: Today we made a plan of approach, made a GitHub repository to store our program.
+We also started working on the game. First, we made the board, then we made 2 players
+and started working on the dice-roll logic. We will continue this later.
--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import random
 
 
 class Game(object):
@@ -14,7 +15,7 @@ class Game(object):
         self.window_height = height
         self.state_stack = []
 
-        self.bg_color = (150, 150, 200)
+        self.bg_color = (150, 150, 170)
         self.text_color = (0, 0, 0)
         self.color_red = (255, 0, 0)
         self.color_purple = (112, 48, 160)
@@ -22,14 +23,29 @@ class Game(object):
         self.color_brown = (198, 89, 17)
         self.color_yellow = (255, 255, 0)
         self.color_green = (146, 208, 80)
+        self.color_gray = (100, 100, 100)
+        self.p1_color = (250, 117, 24)
+        self.p2_color = (252, 15, 192)
+        self.coords = [(500, 560), (450, 560), (400, 560), (350, 560), (300, 560), (250, 560), (200, 560), (150, 560), (100, 560), (50, 560), (50, 510),  # Bottom
+                  (50, 460), (50, 410), (50, 360), (50, 310), (50, 260), (50, 210), (50, 160), (50, 110),  # Left
+                  (100, 110), (150, 110), (200, 110), (250, 110), (300, 110), (350, 110), (400, 110), (450, 110), (500, 110),  # Top
+                  (500, 160), (500, 210), (500, 260), (500, 310), (500, 360), (500, 410), (500, 460), (500, 510)]  # Right
 
         self.clock = pg.time.Clock()
         self.fps = 60
 
         self.font_100 = pg.font.Font("res/fonts/pixeltype.ttf", 100)
         self.font_50 = pg.font.Font("res/fonts/pixeltype.ttf", 50)
+        self.font_24 = pg.font.Font("res/fonts/pixeltype.ttf", 25)
 
         self.pause_icon = pg.image.load("res/icon.png").convert_alpha()
+
+        self.p1_pos = 0  # The tile the player is on, 0 is Start, 35 is max
+        self.p2_pos = 0
+
+        self.d1_roll = 0
+        self.d2_roll = 0
+
 
     # Shows the start menu
     def start(self):
@@ -161,7 +177,18 @@ class Game(object):
 
     # Shows the game
     def game(self):
-        text = self.font_100.render("Game", False, self.text_color).convert()
+        tile_size = 50
+        tiles = ["START", "NONE", "EVENT", "NONE", "NONE", "QUERY", "NONE", "FAMOUS", "NONE", "SHOP", "NONE", "CHANCE", "NONE", "NONE", "QUERY", "NONE", "RANDOM",
+                 "NONE", "JAIL", "NONE", "EVENT", "NONE", "NONE", "QUERY", "NONE", "FAMOUS", "NONE", "SHOP", "NONE", "CHANCE", "NONE", "NONE", "QUERY", "NONE", "RANDOM", "NONE"]
+
+        legend_text = [self.font_24.render("Start", False, self.color_yellow).convert(),
+                       self.font_24.render("Shop", False, self.color_red).convert(),
+                       self.font_24.render("Query", False, self.color_blue).convert(),
+                       self.font_24.render("Chance", False, self.color_purple).convert(),
+                       self.font_24.render("Event", False, self.color_green).convert(),
+                       self.font_24.render("Jail", False, "Black").convert(),
+                       self.font_24.render("Famous Person", False, self.color_gray).convert(),
+                       self.font_24.render("Random Item", False, self.color_brown).convert()]
 
         while self.state_stack[-1] == "GAME":
             for event in pg.event.get():
@@ -173,13 +200,49 @@ class Game(object):
                         # Show pause menu
                         self.state_stack.append("PAUSE")
                         self.pause()
+                    elif event.key == pg.K_SPACE:
+                        self.dice_roll()
+                        print(f"{self.d1_roll} {self.d2_roll}")
+                        break
 
             # Update
 
             # Render
             self.screen.fill(self.bg_color)
-            self.screen.blit(text, text.get_rect(center=(self.window_width / 2, self.window_height / 2)))
+            # Render the board
+            for i in range(len(tiles)):
+                if tiles[i] == "NONE":
+                    pg.draw.rect(self.screen, "White", pg.Rect(self.coords[i][0], self.coords[i][1], tile_size, tile_size))
+                elif tiles[i] == "JAIL":
+                    pg.draw.rect(self.screen, "Black", pg.Rect(self.coords[i][0], self.coords[i][1], tile_size, tile_size))
+                elif tiles[i] == "START":
+                    pg.draw.rect(self.screen, self.color_yellow, pg.Rect(self.coords[i][0], self.coords[i][1], tile_size, tile_size))
+                elif tiles[i] == "SHOP":
+                    pg.draw.rect(self.screen, self.color_red, pg.Rect(self.coords[i][0], self.coords[i][1], tile_size, tile_size))
+                elif tiles[i] == "QUERY":
+                    pg.draw.rect(self.screen, self.color_blue, pg.Rect(self.coords[i][0], self.coords[i][1], tile_size, tile_size))
+                elif tiles[i] == "CHANCE":
+                    pg.draw.rect(self.screen, self.color_purple, pg.Rect(self.coords[i][0], self.coords[i][1], tile_size, tile_size))
+                elif tiles[i] == "EVENT":
+                    pg.draw.rect(self.screen, self.color_green, pg.Rect(self.coords[i][0], self.coords[i][1], tile_size, tile_size))
+                elif tiles[i] == "FAMOUS":
+                    pg.draw.rect(self.screen, self.color_gray, pg.Rect(self.coords[i][0], self.coords[i][1], tile_size, tile_size))
+                elif tiles[i] == "RANDOM":
+                    pg.draw.rect(self.screen, self.color_brown, pg.Rect(self.coords[i][0], self.coords[i][1], tile_size, tile_size))
+
+            # Render the legend
+            for i in range(len(legend_text)):
+                self.screen.blit(legend_text[i], legend_text[i].get_rect(topleft=(600, (i * 50) + 175)))
+
+            # Render the players
+            pg.draw.circle(self.screen, self.p1_color, (self.coords[self.p1_pos][0] + 15, self.coords[self.p1_pos][1] + 15), 10)
+            pg.draw.circle(self.screen, (70, 70, 70), (self.coords[self.p1_pos][0] + 15, self.coords[self.p1_pos][1] + 15), 10, 2)
+            pg.draw.circle(self.screen, self.p2_color,(self.coords[self.p2_pos][0] + 35, self.coords[self.p2_pos][1] + 35), 10)
+            pg.draw.circle(self.screen, (70, 70, 70), (self.coords[self.p2_pos][0] + 35, self.coords[self.p2_pos][1] + 35), 10, 2)
 
             pg.display.update()
             self.clock.tick(self.fps)
-    
+
+    def dice_roll(self):
+        self.d1_roll = random.randint(1, 6)
+        self.d2_roll = random.randint(1, 6)
