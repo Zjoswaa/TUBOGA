97fbe9b666fd99e30585edfd76dcafd0a7b550bc
rydbl <159252313+rydbl@users.noreply.github.com>
2024-03-06 00:24:37 +0100

    Add files via upload

---
+++ questions.py
@@ -0,0 +1,51 @@
+# Questions
+questions = [
+    "What is the primary function of a CPU?",
+    "Which of the following is NOT a type of computer memory?",
+    "Which programming language is renowned for its readability and simplicity?",
+    "What does IDE stand for in the context of programming?",
+    "What file extension is commonly used for Python files?",
+    "What is the purpose of the 'if' statement in Python?",
+    "Which of the following is used to indicate a single-line comment in Python?",
+    "What does HTML stand for?",
+    "What is the purpose of a 'for' loop in Python?",
+    "Which of the following is a data type in Python?",
+    "Which programming paradigm does Python primarily follow?",
+    "What symbol is used for exponentiation in Python?",
+    "Which function is used to read input from the user in Python?",
+    "Which of the following is NOT a valid Python variable name?",
+    "Which operator is used for floor division in Python?",
+    "What is the output of 2 + 3 * 5?",
+    "Which of the following data structures stores elements in key-value pairs?",
+    "What does the 'len()' function in Python return?",
+    "Which of the following is used to import a module in Python?",
+    "What does the 'append()' method do in Python lists?"
+]
+
+# Choices
+choices = [
+    ["a) Storing data", "b) Processing instructions", "c) Providing power"],
+    ["a) RAM", "b) ROM", "c) CPU"],
+    ["a) C++", "b) Python", "c) Java"],
+    ["a) Integrated Development Environment", "b) Internet Development Environment", "c) Integrated Design Environment"],
+    ["a) .py", "b) .java", "c) .cpp"],
+    ["a) Iterating over a sequence", "b) Executing code based on conditions", "c) Defining a function"],
+    ["a) //", "b) --", "c) #"],
+    ["a) HyperText Markup Language", "b) HighTech Multimedia Language", "c) Hyperlink and Text Management Language"],
+    ["a) Repeating a block of code a fixed number of times", "b) Iterating over a sequence of elements", "c) Making decisions based on conditions"],
+    ["a) String", "b) Function", "c) Variable"],
+    ["a) Procedural", "b) Object-Oriented", "c) Functional"],
+    ["a) ^", "b) **", "c) %"],
+    ["a) input()", "b) read()", "c) get_input()"],
+    ["a) my_variable", "b) 2variable", "c) variable_2"],
+    ["a) /", "b) //", "c) %"],
+    ["a) 10", "b) 25", "c) 17"],
+    ["a) List", "b) Dictionary", "c) Tuple"],
+    ["a) The length of a string", "b) The number of elements in a list", "c) Both a and b"],
+    ["a) require", "b) import", "c) include"],
+    ["a) Removes an element from the list", "b) Adds an element to the end of the list", "c) Sorts the list in ascending order"]
+]
+
+# Answers
+answers = [1, 2, 1, 0, 0, 1, 2, 0, 1, 0, 1, 1, 0, 1, 1, 2, 1, 2, 1, 1]
+          
